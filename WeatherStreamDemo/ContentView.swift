010cc927bcaa321436ebc44bbac12f6d512adb74
Wes Matlock <wmatlock@gmail.com>
2025-09-26 04:22:56 -0500

    Initial Commit

---
+++ WeatherStreamDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherStreamDemo
+//
+//  Created by Wesley Matlock on 9/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherStreamDemo/WeatherStreamDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherStreamDemoApp.swift
+//  WeatherStreamDemo
+//
+//  Created by Wesley Matlock on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherStreamDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
